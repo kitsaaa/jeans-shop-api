e26cf2a130c6329889d3f2543d6ff327dfcdab7e
Nikita Korolkov <nikitakorolkov@Nikitas-MacBook-Pro.local>
2023-01-09 23:23:29 -0500

    initial commit

---
+++ modules/post-schema.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+let Schema = mongoose.Schema;
+
+let postSchema = new Schema({
+    _id: String,
+    title: String,
+    price: String,
+    images: [String],
+    about: String
+}, {
+    versionKey: false
+});
+
+module.exports = postSchema;
